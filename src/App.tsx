dcbbb0a637ec89a060cbbb1930d23977e0c8717b
yvwbimpong <bimpongamoako@gmail.com>
2024-08-09 14:28:14 +0000

    added comms

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import './index.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import './App.css';
 import FrlLanding from './sandboxes/FRL';
 import DocxUploader from './sandboxes/OCM';
 import CSVReader from './sandboxes/CSVReader';
+import DocUploader from './screens/UploadFile';
+import InAppSpy from 'inapp-spy';
+import { Typography } from '@mui/material';
+import ViteView from './sandboxes/Vite';
+
+
 
 function App() {
-	return <DocxUploader />;
-	return <CSVReader />;
-	return <FrlLanding />;
+	const { isInApp, appKey, appName, ...rest } = InAppSpy();
+	
+	console.log({isInApp, appKey, appName, rest})
+	
+	const [inAppState, setInAppState] = useState({})
+	
+	useEffect(()=>{
+		
+		alert('is in app' + JSON.stringify({isInApp, appName,appKey}))
+		
+		setInAppState({ isInApp, appName, appKey, ...rest });
+		
+	}, [isInApp, appName, appKey])
+	
+	
+	return <>
+	<Typography className='text-black'>
+		{JSON.stringify(inAppState)}
+	</Typography>
+	<ViteView />
+	</>
+	// return <DocxUploader />;
+	// return <CSVReader />;
+	// return <FrlLanding />;
 }
 
 export default App;
---
+++ src/sandboxes/OCM/readme.md
@@ -0,0 +1,14 @@
+1 - Upload Comms - copy paste into text area or file picker or drag and drop
+
+2 - Check Comms - Provide suggestions for copy writer to review their changes against some suggestions/guidelines before proceeding
+
+3 - Readability Checks - Get readability scores and make modifications to improve it before submitting to model
+
+4 - Model Analysis - Get analysis on comms thus far and allow writer to make further modifications to comms based on analysis and analyse again until achieved desired comms
+
+5 - Analysis and suggestion to be reviewed by some approver?
+Accept/Finalize comms - Page for comparison between refined vs original communication
+
+6 - user feedback on final comms??? (answered now)
+
+7 - Also if we are version controlling the input we might also want to version control the output so we can know what readability scores or model analysis came along with each version?
